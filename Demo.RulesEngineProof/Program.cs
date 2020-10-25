44615d03da19451c660c1e641dbc6b17381e2ead
Todd Meinershagen <todd@meinershagen.net>
2020-10-25 12:43:30 -0500

    Added to demonstrate the need for strongly typed objects if using properties that are complex or list-based to help the interpreter out.

--- Demo.RulesEngineProof/Program.cs
+++ Demo.RulesEngineProof/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Dynamic;
+using System.Linq;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 
@@ -40,17 +42,24 @@ namespace Demo.RulesEngineProof
             var basicInfo = "{\"name\": \"Dishant\",\"email\": \"dishantmunjal@live.com\",\"creditHistory\": \"good\",\"country\": \"india\",\"loyalityFactor\": 3,\"totalPurchasesToDate\": 10000}";
             var orderInfo = "{\"totalOrders\": 5,\"recurringItems\": 2}";
             var telemetryInfo = "{\"noOfVisitsPerMonth\": 10,\"percentageOfBuyingToVisit\": 15}";
+            var employeeInfo = "{\"employees\":[{ \"Name\":\"Siddiq\",\"Age\":31}, { \"Name\":\"Jason\",\"Age\":45}]}"; //never used because the dynamic interpreter can't handle nested levels of lists.
 
             var converter = new ExpandoObjectConverter();
             dynamic input1 = JsonConvert.DeserializeObject<ExpandoObject>(basicInfo, converter);
             dynamic input2 = JsonConvert.DeserializeObject<ExpandoObject>(orderInfo, converter);
             dynamic input3 = JsonConvert.DeserializeObject<ExpandoObject>(telemetryInfo, converter);
+            var input4 = new List<Input.Employee> 
+            { 
+                new Input.Employee { Name = "Siddiq", Age = 31 }, 
+                new Input.Employee { Name = "Jason", Age = 45 } 
+            };
 
             var inputs = new dynamic[]
                 {
                     input1,
                     input2,
-                    input3
+                    input3,
+                    input4
                 };
 
             var repository = new RulesRepository();
@@ -61,5 +70,19 @@ namespace Demo.RulesEngineProof
                 : $"Discount offered is {discountOffered * 100}% over MRP.";
             Console.WriteLine(discountMessage);
         }
+    }
+
+    /// <summary>
+    /// This is just a demonstration of strongly typed objects and that the Engine can handle these.
+    /// </summary>
+    public class Input
+    {
+        public class Employee
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+
+        public List<Employee> Employees { get; set; }
     }
 }
